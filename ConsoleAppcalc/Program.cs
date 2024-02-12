0d0b524cf9055c3c7008518a8c52d4876690c627
Vladislavooi <vlad.buzin.2018@bk.ru>
2024-02-12 17:14:28 +0400

    Добавьте файлы проекта.

---
+++ ConsoleAppcalc/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Calculator
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Выберите операцию (+, -, *, /):");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Некорректная операция");
+                break;
+        }
+
+        Console.WriteLine("Результат: " + result);
+    }
+}
